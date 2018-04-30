5701010ccfb2fe521be0d4b05943efa27e94c12f
Brian Torluemke <briantorluemke@gmail.com>
2018-04-30 12:04:19 -0500

    Finished first and second Java Exercises for codeup

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        int myFavoriteNumber = 7;
+        String myString = "Goodbye";
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        long myNumber;
+        myNumber = 123;
+        System.out.println(myNumber);
+        int x = 10;
+        int y = 2;
+        x /= y;
+        System.out.println(x);
+        y -= x;
+        System.out.println(y);
+    }
+}
