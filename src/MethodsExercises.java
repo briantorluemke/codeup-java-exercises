0dad12ee2e4f070fe21df6cdd10bee00d543c254
Brian Torluemke <briantorluemke@gmail.com>
2018-05-02 14:09:01 -0500

    Created factorial and dice roll functions.

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class MethodsExercises {
     public static void main(String[] args) {
@@ -8,8 +9,8 @@ public class MethodsExercises {
 //        divide(200,5);
 //        mod(60,13);
 //        getInteger(1,10);
-
-
+//        fact();
+//        roll();
     }
 
     public static int add(int x, int y) {
@@ -58,5 +59,41 @@ public class MethodsExercises {
             getInteger(x,y);
         }
         return input;
+    }
+
+    public static int fact() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 10: ");
+        int input = sc.nextInt();
+        if (input>=1 && input<=10) {
+            int fact = 1;
+            for (int i = 1; i <= input; i++) {
+                fact = fact*i;
+            }
+            System.out.println(fact);
+            return fact;
+        } else {
+            System.out.println("That's not between 1 and 10, wtf.");
+            fact();
+        }
+        return input;
+    }
+
+    public static int roll() {
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Would you like to roll?\n  Y  |  N   ");
+        String confirmRoll = sc.next();
+        if (confirmRoll.equals("Y") || confirmRoll.equals("y")) {
+            System.out.print("How many sides should each die have? Enter a number: ");
+            int dieSides = sc.nextInt();
+            System.out.println("You roll the dice...");
+            int x = rand.nextInt(dieSides);
+            int y = rand.nextInt(dieSides);
+            System.out.println("You rolled: "+x+" and "+y);
+        } else {
+            System.out.println("Player opted not to roll");
+        }
+        return 0;
     }
 }
